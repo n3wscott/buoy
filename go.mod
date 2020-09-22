b3217863c4eb95df723f02e62e3f73e428ce56a3
Scott Nichols <snichols@vmware.com>
2020-09-22 15:23:20 -0700

    working poc

---
+++ go.mod
@@ -0,0 +1,11 @@
+module github.com/n3wscott/bouy
+
+go 1.14
+
+require (
+	github.com/blang/semver/v4 v4.0.0
+	github.com/google/go-github/v32 v32.1.0
+	golang.org/x/net v0.0.0-20200202094626-16171245cfb2
+	golang.org/x/oauth2 v0.0.0-20190604053449-0f29369cfe45
+	gopkg.in/yaml.v2 v2.2.8
+)
---
+++ pull.go
@@ -0,0 +1,77 @@
+package main
+
+import (
+	"fmt"
+	"golang.org/x/net/html"
+	"net/http"
+	"strings"
+)
+
+// MetaImport represents the parsed <meta name="go-import"
+// content="prefix vcs reporoot" /> tags from HTML files.
+type MetaImport struct {
+	Prefix, VCS, RepoRoot string
+}
+
+func (m *MetaImport) OrgRepo() (string, string) {
+	repoRoot := strings.TrimSuffix(m.RepoRoot, ".git")
+	repoRoot = strings.TrimPrefix(repoRoot, "https://github.com/")
+
+	parts := strings.Split(repoRoot, "/")
+	if len(parts) == 2 {
+		return parts[0], parts[1]
+	}
+	panic(fmt.Errorf("unknown repo root: %s", m.RepoRoot))
+}
+
+func metaContent(doc *html.Node, name string) (string, error) {
+	var meta *html.Node
+	var crawler func(*html.Node)
+	crawler = func(node *html.Node) {
+		if node.Type == html.ElementNode && node.Data == "meta" {
+			for _, attr := range node.Attr {
+				if attr.Key == "name" && attr.Val == name {
+					meta = node
+					return
+				}
+			}
+
+		}
+		for child := node.FirstChild; child != nil; child = child.NextSibling {
+			crawler(child)
+		}
+	}
+	crawler(doc)
+	if meta != nil {
+		for _, attr := range meta.Attr {
+			if attr.Key == "content" {
+				return attr.Val, nil
+			}
+		}
+	}
+	return "", fmt.Errorf("missing <meta name=%s> in the node tree", name)
+}
+
+func GetMetaImport(i string) (*MetaImport, error) {
+	url := fmt.Sprintf("https://%s?go-get=1", i)
+	resp, err := http.Get(url)
+	if err != nil {
+		return nil, err
+	}
+	doc, err := html.Parse(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+	content, err := metaContent(doc, "go-import")
+	if err != nil {
+		return nil, err
+	}
+
+	f := strings.Fields(content)
+
+	return &MetaImport{
+		Prefix:   f[0],
+		VCS:      f[1],
+		RepoRoot: f[2],
+	}, nil
+}
